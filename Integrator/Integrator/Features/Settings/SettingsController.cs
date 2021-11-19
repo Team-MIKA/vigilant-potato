df5d5983eac76233b40e5597f2da48565950141c
Kasper Stenholt Winkler <kasperstenholtwinkler@gmail.com>
2021-11-19 10:56:08 +0100

    Added demo get endpoint to SettingsController.cs

--- Integrator/Integrator/Features/Settings/SettingsController.cs
+++ Integrator/Integrator/Features/Settings/SettingsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Integrator.Features.Settings.DTO;
 using Integrator.Features.Settings.Models;
+using Integrator.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,16 +12,23 @@ namespace Integrator.Features.Settings
     public class SettingsController : ControllerBase
     {
         private readonly ILogger<SettingsController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public SettingsController(ILogger<SettingsController> logger)
+        public SettingsController(ILogger<SettingsController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
         
-        // [HttpGet]
-        // public IEnumerable<Setting> Get()
-        // {
-        //     return ""
-        // }
+        [HttpGet]
+        public IEnumerable<SettingDTO> Get()
+        {
+            return _unitOfWork.Settings.ListAll()
+                .Select(setting => new SettingDTO
+                {
+                    Id = setting.Id, 
+                    Name = setting.Name
+                });
+        }
     }
 }
