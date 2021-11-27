a5edc421a2ea06f52861b20fc5aaea212d0728af
Mads Smed <masm@proactive.dk>
2021-11-27 14:42:04 +0100

    Move namespace of MappingProfiles

--- Integrator/Integrator/Infrastructure/MapperProfiles.cs
+++ Integrator/Integrator/Infrastructure/MapperProfiles.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Integrator.Features.Settings.DTO;
 using Integrator.Features.Settings.Models;
 
-namespace Integrator.Features.Settings.Mappings
+namespace Integrator.Infrastructure
 {
     public class MapperProfiles : Profile
     {
