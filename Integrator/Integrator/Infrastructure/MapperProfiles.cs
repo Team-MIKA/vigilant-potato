beae7b80e5d602fd187dbba1be7bac6c9f6f804b
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-29 13:17:49 +0100

    prettier

--- Integrator/Integrator/Infrastructure/MapperProfiles.cs
+++ Integrator/Integrator/Infrastructure/MapperProfiles.cs
@@ -11,19 +11,35 @@ using Integrator.Features.Workspaces.DTO;
 using Integrator.Features.Workspaces.Models;
 
 
-namespace Integrator.Features.Settings.Mappings
+namespace Integrator.Infrastructure
 {
     public class MapperProfiles : Profile
     {
         public MapperProfiles()
         {
             CreateMap<Setting, SettingDTO>().ReverseMap();
+            
             CreateMap<Workspace, WorkspaceDTO>().ForMember(dst => dst.Widgets, opt => opt.MapFrom(src => src.Widgets)).ReverseMap();
+            
+            
             CreateMap<WorkspaceWidget, WidgetDTO>().IncludeMembers(src => src.Widget);
-            CreateMap<WidgetDTO, WorkspaceWidget>().ForMember(dest => dest.Id, act => act.Ignore()).ForMember(dest => dest.Widget, act => act.Ignore()).ForMember(dest => dest.WidgetId, opt => opt.MapFrom(src => src.Id));
+            
+            CreateMap<WidgetDTO, WorkspaceWidget>()
+                .ForMember(dest => dest.Id, act => act.Ignore())
+                .ForMember(dest => dest.Widget, act => act.Ignore())
+                .ForMember(dest => dest.WidgetId, opt => opt
+                    .MapFrom(src => src.Id));
+            
             CreateMap<WorkspaceWidget, WorkspaceDTO>().IncludeMembers(src => src.Workspace);
-            CreateMap<WorkspaceDTO, WorkspaceWidget>().ForMember(dest => dest.Id, act => act.Ignore()).ForMember(dest => dest.Workspace, act => act.Ignore()).ForMember(dest => dest.WorkspaceId, opt => opt.MapFrom(src => src.Id));
+            
+            CreateMap<WorkspaceDTO, WorkspaceWidget>()
+                .ForMember(dest => dest.Id, act => act.Ignore())
+                .ForMember(dest => dest.Workspace, act => act.Ignore())
+                .ForMember(dest => dest.WorkspaceId, opt => opt
+                    .MapFrom(src => src.Id));
 
+            
+            
             // CreateMap<WidgetDTO, WorkspaceWidget>().ForSourceMember()
             //.ForMember(dest => dest.WidgetId, opt => opt.MapFrom(src => src.Id)).ForMember(dest => dest.WorkspaceId, opt => opt.MapFrom(src => src.Id));
             CreateMap<Widget, WidgetDTO>().ReverseMap();
