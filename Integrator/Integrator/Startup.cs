594fb56779ebf57c959b91b3ab8cfa9d70818b59
Mads Smed <masm@proactive.dk>
2021-11-29 17:34:53 +0100

    Remove HttpsRedirect and exposure of port 443

--- Integrator/Integrator/Startup.cs
+++ Integrator/Integrator/Startup.cs
@@ -54,7 +54,7 @@ namespace Integrator
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Integrator v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
