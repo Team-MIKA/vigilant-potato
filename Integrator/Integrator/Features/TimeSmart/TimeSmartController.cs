0ec680194f619724c31b785e55e35cd6dc1f78a3
Mads Smed <masm@proactive.dk>
2021-11-29 07:36:38 +0100

    Add Category Controller

---
+++ Integrator/Integrator/Features/TimeSmart/TimeSmartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Integrator.Features.Settings;
+using Integrator.Features.TimeSmart.DTO;
+using Integrator.Features.TimeSmart.Models;
+using Integrator.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Integrator.Features.TimeSmart
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeSmartController : ControllerBase
+    {
+        private readonly ILogger<SettingsController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public TimeSmartController(ILogger<SettingsController> logger, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public IEnumerable<CategoryDto> Get()
+        {
+            var categories = _unitOfWork.Categories.ListAll();
+            var categoryDtos = _mapper.Map<IEnumerable<CategoryDto>>(categories);
+
+            return categoryDtos;
+        }
+
+        [HttpPost]
+        public void Insert([FromBody] CategoryDto newCategory)
+        {
+            var category = _mapper.Map<RegistrationCategory>(newCategory);
+            category.Id = Guid.NewGuid().ToString();
+            _unitOfWork.Categories.Insert(category);
+            _unitOfWork.Complete();
+        }
+    }
+}
