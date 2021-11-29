866096cc7113fe84eaffdcf779532d4a48d2ae7d
Kasper Stenholt Winkler <kasperstenholtwinkler@gmail.com>
2021-11-29 10:11:13 +0100

    Initial service layer commit

---
+++ Integrator/Integrator/Features/Settings/ISettingsService.cs
@@ -0,0 +1,9 @@
+﻿using Integrator.Features.Settings.DTO;
+
+namespace Integrator.Features.Settings
+{
+    public interface ISettingsService
+    {
+        void CreateSetting(SettingDTO setting);
+    }
+}
---
+++ Integrator/Integrator/Features/Settings/SettingsService.cs
@@ -0,0 +1,12 @@
+﻿using Integrator.Features.Settings.DTO;
+
+namespace Integrator.Features.Settings
+{
+    public class SettingsService : ISettingsService
+    {
+        public void CreateSetting(SettingDTO setting)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
